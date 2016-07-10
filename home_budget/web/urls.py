289991c0e3e7858f23cfbf10c4333394ea5a7764
Adrian G Prado <adrian@garciaprado.es>
2016-07-09 21:16:06 -0500

    Django add app in settings and redirect url

--- home_budget/web/urls.py
+++ home_budget/web/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
 
-    url(r'^polls/', include('apps.budget.urls')),
+    url(r'^', include('apps.budget.urls')),
 
     url(r'^admin/', admin.site.urls),
 ]
