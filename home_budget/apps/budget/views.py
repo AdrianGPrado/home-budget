134bb72a56d926348cffef9fadae45cc2e9f91d5
Adrian G Prado <adrian@garciaprado.es>
2016-07-12 21:48:13 -0500

    Django All links functional

--- home_budget/apps/budget/views.py
+++ home_budget/apps/budget/views.py
@@ -5,3 +5,31 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'index.html', context)
+
+def index_rtl(request):
+    context = {}
+    return render(request, 'index-rtl.html', context)
+
+def charts(request):
+    context = {}
+    return render(request, 'charts.html', context)
+
+def tables(request):
+    context = {}
+    return render(request, 'tables.html', context)
+
+def blank_page(request):
+    context = {}
+    return render(request, 'blank-page.html', context)
+
+def forms(request):
+    context = {}
+    return render(request, 'forms.html', context)
+
+def bootstrap_elements(request):
+    context = {}
+    return render(request, 'bootstrap-elements.html', context)
+
+def bootstrap_grid(request):
+    context = {}
+    return render(request, 'bootstrap-grid.html', context)
